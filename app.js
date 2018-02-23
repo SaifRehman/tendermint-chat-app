116062724057d1724908c96915107e415a751cd8
saifrehman <saif.rehman1@ibm.com>
2018-02-23 13:29:35 +0400

    remove function

--- app.js
+++ app.js
@@ -1,31 +1,28 @@
 let lotion = require('lotion');
 let lotionPort = 3000;
-function run() {
-    let opts = {
-        devMode: true,
-        lotionPort: lotionPort,
-        p2pPort: 46656,
-        tendermintPort: 46657,
-        initialState: {
-            messages: []
-        }
-    };
-    let app = lotion(opts)
-    let msgHandler = (state, tx) => {
-        if (typeof tx.sender === 'string' && typeof tx.message === 'string') {
-            if (tx.message !== '') {
-                state.messages.push({
-                    sender: tx.sender,
-                    message: tx.message
-                });
-            }
+let opts = {
+    devMode: true,
+    lotionPort: lotionPort,
+    p2pPort: 46656,
+    tendermintPort: 46657,
+    initialState: {
+        messages: []
+    }
+};
+let app = lotion(opts)
+let msgHandler = (state, tx) => {
+    if (typeof tx.sender === 'string' && typeof tx.message === 'string') {
+        if (tx.message !== '') {
+            state.messages.push({
+                sender: tx.sender,
+                message: tx.message
+            });
         }
     }
-    app.use(msgHandler);
-    app.listen(lotionPort).then(genesis => {
-    console.log(genesis);
-    }, err => {
-        console.log(err);
-    })
 }
-run()
+app.use(msgHandler);
+app.listen(lotionPort).then(genesis => {
+    console.log(genesis);
+}, err => {
+    console.log(err);
+})
