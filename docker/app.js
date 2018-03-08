9e65f54a31412fee1a4ee1f4c2fd68ae172ebb77
saifrehman <saif.rehman1@ibm.com>
2018-03-08 10:21:22 +0400

    added docker to another file

---
+++ docker/app.js
@@ -0,0 +1,17 @@
+let lotion = require('lotion');
+let lotionPort = 3000;
+let opts = {
+    lotionPort: lotionPort,
+    p2pPort: 46656,
+    tendermintPort: 46657,
+    abciPort:35597,
+    initialState: {
+        messages: []
+    }
+};
+let lotionapp = lotion(opts)
+lotionapp.listen(lotionPort).then(genesis => {
+    console.log(genesis);
+}, err => {
+    console.log(err);
+})
