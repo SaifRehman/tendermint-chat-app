574ce19a944dfb62332726a4702964c434034555
admin <admin@admins-MacBook-Pro.local>
2018-03-10 00:12:24 +0700

    Made er print the GCI, and standardized language.

--- blockchain/app.js
+++ blockchain/app.js
@@ -1,4 +1,5 @@
 let lotion = require('lotion');
+let shea = require('shea')
 let lotionPort = 3000;
 let opts = {
     lotionPort: lotionPort,
@@ -9,8 +10,8 @@ let opts = {
         messages: []
     }
 };
-let lotionapp = lotion(opts)
-lotionapp.use(function (state, tx) {
+let app = lotion(opts)
+app.use(function (state, tx) {
         if (tx.message !== '') {
             state.messages.push({
                 sender: tx.sender,
@@ -18,8 +19,9 @@ lotionapp.use(function (state, tx) {
             });
         }
   })
-lotionapp.listen(lotionPort).then(genesis => {
+app.listen(lotionPort).then(genesis => {
     console.log(genesis);
+    console.log(appInfo.GCI)
 }, err => {
     console.log(err);
 })
