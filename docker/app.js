caa0f5243ea147013ae48005fe5df55f5f3baeac
saifrehman <saif.rehman1@ibm.com>
2018-03-09 19:13:50 +0400

    fixed code

--- docker/app.js
+++ docker/app.js
@@ -9,17 +9,12 @@ app.use((state, tx) => {
     state.messages.push({ sender: tx.sender, message: tx.message })
   }
 })
-
 app.use(shea('./public/index.html'))
 app.listen(3000)
-
 var http = require('http');
-
 var finalhandler = require('finalhandler');
 var serveStatic = require('serve-static');
-
 var serve = serveStatic("./");
-
 var server = http.createServer(function(req, res) {
   var done = finalhandler(req, res);
   serve(req, res, done);
