66d0d45c92423e2511d6e22cea1c9f55f987eaa7
saifrehman <saif.rehman1@ibm.com>
2018-03-10 17:35:42 +0400

    clean

--- docker/app.js
+++ docker/app.js
@@ -30,24 +30,7 @@ expressapp.use(bodyParser.urlencoded({ extended: true }));
 expressapp.use(bodyParser.json());
 expressapp.use(cors({credentials: true, origin: true}))
 expressapp.use(function (req, res, next) {
-  // Website you wish to allow to connect
- // Website you wish to allow to connect
- res.setHeader('Access-Control-Allow-Origin', '*');
-
- // Request methods you wish to allow
- res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
- // Request headers you wish to allow
- res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); 
-
-// Set to true if you need the website to include cookies in the requests sent 
-
-// to the API (e.g. in case you use sessions)
- res.setHeader('Access-Control-Allow-Credentials', true); 
-
-// Pass to next layer of middleware 
-
-// next();
+ res.setHeader('Access-Control-Allow-Headers', "*");
 });
 var port =  8888;       
 var router = express.Router();              // get an instance of the express Router
@@ -84,24 +67,11 @@ expressapp.listen(port)
 console.log('Magic happens on port ' + port);
 
 var server = http.createServer(function(req, res) {
-  // Website you wish to allow to connect
   res.setHeader('Access-Control-Allow-Origin', '*');
- 
-  // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
- 
-  // Request headers you wish to allow
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); 
- 
- // Set to true if you need the website to include cookies in the requests sent 
- 
- // to the API (e.g. in case you use sessions)
   res.setHeader('Access-Control-Allow-Credentials', true); 
- 
- // Pass to next layer of middleware 
- 
- // next();
-     if ( req.method === 'OPTIONS' ) {
+      if ( req.method === 'OPTIONS' ) {
        res.writeHead(200);
        res.end();
        return;
