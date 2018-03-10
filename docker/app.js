6e633549c172400313c12c822269bc3d0431d3ef
saifrehman <saif.rehman1@ibm.com>
2018-03-10 09:00:31 +0400

    uodated with asynch

--- docker/app.js
+++ docker/app.js
@@ -1,27 +1,44 @@
 let shea = require('shea')
 
 let app = require('lotion')({
-  tendermintPort:46657,
+  lotionPort: 3000,
+  p2pPort: 46656,
+  tendermintPort: 46657,
   initialState: { messages: [] },
 })
 
-app.use((state, tx) => {
-  if (typeof tx.sender === 'string' && typeof tx.message === 'string') {
-    state.messages.push({ sender: tx.sender, message: tx.message })
-  }
-})
 
-app.listen(3000).then(function(data){
-  console.log('data iss',data)
-})
-var http = require('http');
-var finalhandler = require('finalhandler');
-var serveStatic = require('serve-static');
-var serve = serveStatic("./");
-var server = http.createServer(function(req, res) {
-  var done = finalhandler(req, res);
-  serve(req, res, done);
+async function main() {
+  app.use((state, tx) => {
+    if (typeof tx.sender === 'string' && typeof tx.message === 'string') {
+      state.messages.push({ sender: tx.sender, message: tx.message })
+    }
+  })
+  
+  app.listen(3000).then(function(data){
+    console.log('data iss',data)
+  })
+  var http = require('http');
+  var finalhandler = require('finalhandler');
+  var serveStatic = require('serve-static');
+  var serve = serveStatic("./");
+  var server = http.createServer(function(req, res) {
+    var done = finalhandler(req, res);
+    serve(req, res, done);
+  });
+  server.listen(8000);
+
+  process.on('unhandledRejection', function(reason, p){
+    console.log('Please report the following error as a Github Issue on: ')
+    console.log(
+        ` 
+        Please report the following error as a Github Issue on:
+        https://github.com/devslopes/blockchat
+        `
+    )
+    console.log("Possibly Unhandled Rejection at: Promise ", p, " reason: ", reason);
+    console.trace();
 });
+}
 
-
-server.listen(8000);
+main()
