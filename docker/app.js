b7f8e5c62684e6b8b101c739807dceb285d494d0
saifrehman <saif.rehman1@ibm.com>
2018-03-12 12:48:41 +0400

    #8 all apis done

--- docker/app.js
+++ docker/app.js
@@ -65,6 +65,200 @@ expressapp.get('/api/unconfirmed_txs', cors(), function (req, res) {
   })
 });
 
+expressapp.post('/api/abci_query', cors(), function (req, res) {
+  var path = req.body.path;
+  var data = req.body.data;
+  var height = req.body.height;
+  var prove = req.body.prove;
+  var link = "http://127.0.0.1:46657/abci_query?"
+  if(path !== undefined && path!==null){
+    link = link + "&path=" + '"'+ path + '"'
+  }
+  if(data !== undefined && data!==null){
+    link = link + "&data=" +  '"'+ data + '"'
+  }
+  if(height !== undefined && height!==null){
+    link = link + "&height=" + height
+  }
+  if(prove !== undefined && prove!==null){
+    link = link + "&prove=" + prove
+  }
+  request(link, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.json(JSON.parse(body));
+    }
+  })
+});
+
+expressapp.post('/api/block', cors(), function (req, res) {
+  var height = req.body.height;
+  var link = "http://127.0.0.1:46657/block?"
+  if(height !== undefined && height!==null){
+    link = link + "&height=" + height
+  }
+  request(link, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.json(JSON.parse(body));
+    }
+  })
+});
+
+expressapp.post('/api/block_results', cors(), function (req, res) {
+  var height = req.body.height;
+  var link = "http://127.0.0.1:46657/block_results?"
+  if(height !== undefined && height!==null){
+    link = link + "&height=" + height
+  }
+  request(link, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.json(JSON.parse(body));
+    }
+  })
+});
+
+expressapp.post('/api/blockchain', cors(), function (req, res) {
+  var minHeight = req.body.minHeight;
+  var maxHeight = req.body.maxHeight;
+  var link = "http://127.0.0.1:46657/blockchain?"
+  if(minHeight !== undefined && minHeight!==null){
+    link = link + "&minHeight=" + minHeight
+  }
+  if(maxHeight !== undefined && maxHeight!==null){
+    link = link + "&maxHeight=" + maxHeight
+  }
+  request(link, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.json(JSON.parse(body));
+    }
+  })
+});
+
+
+expressapp.post('/api/broadcast_tx_async', cors(), function (req, res) {
+  var tx = req.body.tx;
+  var link = "http://127.0.0.1:46657/broadcast_tx_async?"
+  if(tx !== undefined && tx!==null){
+    link = link + "&tx=" + '"'+tx +'"'
+  }
+  request(link, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.json(JSON.parse(body));
+    }
+  })
+});
+
+expressapp.post('/api/broadcast_tx_commit', cors(), function (req, res) {
+  var tx = req.body.tx;
+  var link = "http://127.0.0.1:46657/broadcast_tx_commit?"
+  if(tx !== undefined && tx!==null){
+    link = link + "&tx=" + '"'+tx +'"'
+  }
+  request(link, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.json(JSON.parse(body));
+    }
+  })
+});
+
+expressapp.post('/api/broadcast_tx_sync', cors(), function (req, res) {
+  var tx = req.body.tx;
+  var link = "http://127.0.0.1:46657/broadcast_tx_sync?"
+  if(tx !== undefined && tx!==null){
+    link = link + "&tx=" + '"'+tx +'"'
+  }
+  request(link, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.json(JSON.parse(body));
+    }
+  })
+});
+
+expressapp.post('/api/commit', cors(), function (req, res) {
+  var height = req.body.height;
+  var link = "http://127.0.0.1:46657/commit?"
+  if(height !== undefined && height!==null){
+    link = link + "&height=" + height
+  }
+  request(link, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.json(JSON.parse(body));
+    }
+  })
+});
+
+expressapp.post('/api/subscribe', cors(), function (req, res) {
+  var query = req.body.query;
+  var link = "http://127.0.0.1:46657/subscribe?"
+  if(query !== undefined && query!==null){
+    link = link + "&query=" + '"'+query+'"'
+  }
+  request(link, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.json(JSON.parse(body));
+    }
+  })
+});
+
+expressapp.post('/api/tx', cors(), function (req, res) {
+  var hash = req.body.hash;
+  var prove = req.body.prove;
+  var link = "http://127.0.0.1:46657/tx?"
+  if(hash !== undefined && hash!==null){
+    link = link + "&hash=" + '"'+hash+'"'
+  }
+  if(prove !== undefined && prove!==null){
+    link = link + "&prove=" + '"'+prove+'"'
+  }
+  request(link, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.json(JSON.parse(body));
+    }
+  })
+});
+
+expressapp.post('/api/tx_search', cors(), function (req, res) {
+  var query = req.body.query;
+  var prove = req.body.prove;
+  var link = "http://127.0.0.1:46657/tx_search?"
+  if(query !== undefined && query!==null){
+    link = link + "&query=" + '"'+query+'"'
+  }
+  if(prove !== undefined && prove!==null){
+    link = link + "&prove=" + '"'+prove+'"'
+  }
+  request(link, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.json(JSON.parse(body));
+    }
+  })
+});
+
+expressapp.post('/api/unsubscribe', cors(), function (req, res) {
+  var query = req.body.query;
+  var link = "http://127.0.0.1:46657/unsubscribe?"
+  if(query !== undefined && query!==null){
+    link = link + "&query=" + query
+  }
+  request(link, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.json(JSON.parse(body));
+    }
+  })
+});
+
+expressapp.post('/api/commit', cors(), function (req, res) {
+  var height = req.body.height;
+  var link = "http://127.0.0.1:46657/commit?"
+  if(height !== undefined && height!==null){
+    link = link + "&height=" + height
+  }
+  request(link, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.json(JSON.parse(body));
+    }
+  })
+});
+
 expressapp.get('/api/get', cors(), function (req, res) {
   request('http://127.0.0.1:3000/state', function (error, response, body) {
     if (!error && response.statusCode == 200) {
