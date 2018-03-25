5e78168c2db47d9b5e19bb405b88d815398f8cda
saifrehman <saif.rehman1@ibm.com>
2018-03-25 21:24:46 +0400

    small corrections

--- docker/node.js
+++ docker/node.js
@@ -1,7 +1,8 @@
 let { connect } = require('lotion')
 async function main() {
-  let { send } = await connect(null, { genesis: require('./genesis.json'), nodes: ['ws://localhost:46657','ws://localhost:46658','ws://localhost:46659','ws://localhost:46655'] })
+  let { send } = await connect(null, { genesis: require('./genesis.json'), nodes: ['ws://localhost:46659','ws://localhost:46657','ws://localhost:46658','ws://localhost:46655'] })
   console.log(await send({ sender: 'keppel', message: 'hey saif' }))
+  console.log('exitttttttttttttttttttt')
   process.exit();
 }
 main()
