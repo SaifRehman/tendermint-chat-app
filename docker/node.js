796c63e7dd056fc52220d30fec2b0c4774feeab5
SaifRehman <saifrehman@SaifRehmans-MacBook-Pro.local>
2018-03-26 13:48:41 +0400

    a

--- docker/node.js
+++ docker/node.js
@@ -1,10 +1,10 @@
 let app = require('lotion')({
-    genesis: './genesis.json',
+    // genesis: './genesis.json',
     tendermintPort: 46659,
     initialState: { messages: [] },
     p2pPort: 46662,
     devMode: true,
-    peers: ['localhost:46660','localhost:46661']
+    // peers: ['localhost:46660','localhost:46661']
   })
   app.use((state, tx) => {
     if (typeof tx.sender === 'string' && typeof tx.message === 'string') {
@@ -14,3 +14,5 @@ let app = require('lotion')({
   app.listen(3003).then(({ GCI }) => {
     console.log(GCI)
   })
+
+
