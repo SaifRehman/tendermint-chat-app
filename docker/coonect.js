8bf624b9cddb3ec432b001d3fc8d39d641c0f69c
SaifRehman <s4saif.121@gmail.com>
2018-04-01 14:12:11 +0400

    added node3

--- docker/coonect.js
+++ docker/coonect.js
@@ -3,11 +3,7 @@ let app = require('lotion')({
   initialState: { messages: [] },
   tendermintPort: 46657,
   logTendermint: true,
-<<<<<<< HEAD
   peers: ['ws://159.122.175.154:30092','ws://184.173.1.108:30092'],
-=======
-  peers: ['ws://184.173.1.108:30090','ws://159.122.175.154:30090',"ws://168.1.149.33:30090"],
->>>>>>> e91a95a114fd966b690a7a84732fea34f6d5fbcc
 })
 app.use((state, tx) => {
   if (typeof tx.sender === 'string' && typeof tx.message === 'string') {
