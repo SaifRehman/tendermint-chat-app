b1707e19349fa81747bc247ae2d030464e4990e2
SaifRehman <s4saif.121@gmail.com>
2018-04-01 14:01:41 +0400

    fixed master

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { LoginPage } from '../pages/login/login';
+
 @Component({
   templateUrl: 'app.html'
 })
