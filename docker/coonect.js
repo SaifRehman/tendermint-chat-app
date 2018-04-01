e91a95a114fd966b690a7a84732fea34f6d5fbcc
sudoalgorithm <me@knrmalhotra.com>
2018-04-01 13:44:41 +0400

    Changes

--- docker/coonect.js
+++ docker/coonect.js
@@ -3,7 +3,7 @@ let app = require('lotion')({
   initialState: { messages: [] },
   tendermintPort: 46657,
   logTendermint: true,
-  peers: ['ws://184.173.1.108:30090','ws://159.122.175.154:30090'],
+  peers: ['ws://184.173.1.108:30090','ws://159.122.175.154:30090',"ws://168.1.149.33:30090"],
 })
 app.use((state, tx) => {
   if (typeof tx.sender === 'string' && typeof tx.message === 'string') {
