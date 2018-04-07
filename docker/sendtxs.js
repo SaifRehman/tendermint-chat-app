7405dd7bdf59a066721ad64c30d122c3c2cd803e
SaifRehman <s4saif.121@gmail.com>
2018-04-07 22:58:52 +0400

    refactored

--- docker/sendtxs.js
+++ docker/sendtxs.js
@@ -1,7 +1,7 @@
 let { connect } = require('lotion')
 async function main() {
   let { send } = await connect(null, { genesis: require('./genesis.json'),
-   nodes: [] })
+   nodes: ['ws://159.122.175.154:30090','ws://184.173.1.108:30090'] })
   console.log(await send({ sender: 'sendername', message: 'message' }))
   process.exit();
 }
