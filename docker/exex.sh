99c739e3c42acf23dc85dd9738fa4b530fc9e477
SaifRehman <s4saif.121@gmail.com>
2018-04-18 13:55:44 +0400

    removed docker folder

--- docker/exex.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-time for i in {1..10}; do
-    seq 10 | parallel -n0 -j5  "curl -H 'Content-Type: application/json' http://localhost:3009/txs -X POST -d '{\"sender\":\"saif\",\"message\":\"hello\"}' -m 2"
-    sleep 1
-  done
