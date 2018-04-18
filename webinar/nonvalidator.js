ed5bd7a17ddf9294f8044033260fbb0b8ddc9827
SaifRehman <s4saif.121@gmail.com>
2018-04-18 21:11:21 +0400

    refactored webinar

--- webinar/nonvalidator.js
+++ webinar/nonvalidator.js
@@ -5,8 +5,7 @@ let app = require('lotion')({
   initialState: { messages: [] },
   tendermintPort: 46657,
   logTendermint: true,
-  peers: ['ws://localhost:30091','ws://localhost:30093'],
-  createEmptyBlocks: false
+  peers: ['ws://159.122.175.154:30092','ws://184.173.1.108:30092']
 })
 app.use((state, tx) => {
   if (typeof tx.sender === 'string' && typeof tx.message === 'string') {
